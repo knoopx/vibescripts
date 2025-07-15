1d64419164ff9848ad596b1362dfb725dfd721a9
Victor Martinez <knoopx@gmail.com>
2025-07-15 20:09:17 +0200

    style(importantize): 💄 Fix indentation in runtimeInputs
    Adjusted the indentation of the runtimeInputs line for consistency and improved readability in the importantize shell application configuration.

--- pkgs/importantize.nix
+++ pkgs/importantize.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}:
 pkgs.writeShellApplication {
   name = "importantize";
-  # runtimeInputs = with pkgs; [nodejs nodePackages.postcss];
+  runtimeInputs = with pkgs; [nodejs nodePackages.postcss];
   text = ''
     export NODE_PATH="${pkgs.nodePackages.postcss}/lib/node_modules"
     node ${./scripts/importantize.js} "$@"
