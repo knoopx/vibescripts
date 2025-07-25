e16057972a828c68e568a958f17b79be14d638ae
Victor Martinez <knoopx@gmail.com>
2025-07-25 18:00:38 +0200

    remove(display): ➖ Remove niri rotate display scripts
    Deleted the niri rotate display desktop items and associated script files. This change was made to clean up unused features and streamline the codebase.
    
    No longer needed as the functionality is not in use.

--- pkgs/scripts/niri-rotate-display.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-output=$(niri msg --json outputs | jq -r 'to_entries[] | select(.value.logical.transform != "Disabled") | .key' | head -n1)
-niri msg output "$output" transform "$1"
